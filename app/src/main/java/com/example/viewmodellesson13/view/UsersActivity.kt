d1dad9b72784dd6478c9da04aa619da4aab83653
metinyildiran <metin.1464@gmail.com>
2023-08-03 16:32:03 +0300

    Finished lesson 13

--- app/src/main/java/com/example/viewmodellesson13/view/UsersActivity.kt
+++ app/src/main/java/com/example/viewmodellesson13/view/UsersActivity.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.example.viewmodellesson13.UserAdapter
 import com.example.viewmodellesson13.databinding.ActivityUsersBinding
 import com.example.viewmodellesson13.viewmodel.UsersViewModel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class UsersActivity : AppCompatActivity() {
